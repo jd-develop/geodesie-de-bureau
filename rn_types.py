a48997de09674afac655931f621e8f1c39b52519
Jean Dubois <jd-dev@laposte.net>
2024-07-31 15:04:46 +0100

    GPL compliance

--- rn_types.py
+++ rn_types.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding=utf-8 -*-
 
+# Géodésie de Bureau - keep a list of the geodetic benchmarks you’ve seen!
+# Copyright (C) 2024  Jean Dubois
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 RN_TYPE_CODE = {
     "000": "Inconnu",
