ceaefccd1e755f5f2f97fd54c35ae7eb7204b43a
Mistera <mistera91minecraft@gmail.com>
2024-08-04 10:33:32 +0200

    Move files in the right directories

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use geodesie_de_bureau::*;
 
 fn main() {
-    dbg!(get_rn_from_rn_identifications_infos(
-        find_matricule_to_use_from_list("T'.D.S3 - 50", &rn_from_matricule("T'.D.S3 - 50"))
+    dbg!(ngf::get_rn_from_rn_identifications_infos(
+        ngf::find_matricule_to_use_from_list(
+            "T'.D.S3 - 50",
+            &ngf::rn_from_matricule("T'.D.S3 - 50")
+        )
     ));
 }
