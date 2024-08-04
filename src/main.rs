88931a63204118cdf41684449f4cabec475ef13c
Mistera <mistera91minecraft@gmail.com>
2024-08-04 19:45:32 +0200

    Add support for command-line argument

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
+use clap::{arg, command, value_parser};
 use geodesie_de_bureau::*;
 use ngf::*;
 
 fn main() {
-    let rn: RepèreNivellement =
-        ngf::get_rn_from_rn_identifications_infos(ngf::find_matricule_to_use_from_list(
-            "T'.D.S3 - 50",
-            &ngf::rn_from_matricule("T'.D.S3 - 50"),
-        ));
-    println!("{}", rn);
+    let matches: clap::ArgMatches = command!()
+        .arg(
+            arg!(
+                -m --matricule <matricule> "Matricule du repère à visualiser"
+            )
+            .required(true)
+            .value_parser(value_parser!(String)),
+        )
+        .get_matches();
+    if let Some(rn_matricule) = matches.get_one::<String>("matricule") {
+        let rn: RepèreNivellement =
+            ngf::get_rn_from_rn_identifications_infos(ngf::find_matricule_to_use_from_list(
+                rn_matricule,
+                &ngf::rn_from_matricule(rn_matricule),
+            ));
+        println!("{}", rn);
+    } else {
+        println!("Please specify a matricule")
+    }
 }
