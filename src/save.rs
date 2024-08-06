e7a64d8e5999c8fef1e2d69d0235c60cc7e38368
Jean Dubois <jd-dev@laposte.net>
2024-08-06 13:18:46 +0100

    Add function to determine config dir

--- src/save.rs
+++ src/save.rs
@@ -1,4 +1,7 @@
 use serde::{Deserialize, Serialize};
+use std::string::String;
+use std::result::Result;
+extern crate shellexpand;
 
 use crate::ngf;
 
@@ -21,3 +24,29 @@ enum Objet {
 
 #[derive(Serialize, Deserialize)]
 struct Visite {}
+
+fn determine_config_directory() -> Result<String, String> {
+    match std::env::consts::OS {
+        "ios" | "android" => Err(
+            "Sorry, 'ios' and 'android' are not supported by Géodésie de Bureau"
+                .to_string()
+        ),
+
+        "macos" => Ok(shellexpand::tilde("~/Library/Preferences/org.jd-develop.geodesie/").to_string()),
+
+        "windows" => {
+            let appdata = match std::env::var("APPDATA") {
+                Ok(value) => value,
+                Err(err) => return Err(err.to_string()) 
+            };
+            Ok(
+                format!("{}\\jd-develop\\geodesie", appdata)
+                .to_string()
+            )
+        },
+
+        _ => Ok(shellexpand::tilde("~/.config/jd-develop/geodesie").to_string()),
+    }
+
+}
+
