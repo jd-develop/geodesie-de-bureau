c6e60a57bdd4890d0feb879dcfb6fd9e4b05714f
Mistera <mistera91minecraft@gmail.com>
2024-08-06 10:09:12 +0200

    save.rs doesn’t belong to this directory (don’t know how I haven’t noticed earlier)

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,2 @@
 pub mod ngf;
+pub mod save;
--- src/ngf/json_mappings/mod.rs
+++ src/ngf/json_mappings/mod.rs
@@ -1,4 +1,3 @@
 //! These are the JSON mappings, used to serialize structs into JSON, and to deserialize JSON into structs
 pub mod bbox;
 pub mod repere;
-pub mod save;
